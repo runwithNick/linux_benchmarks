d9f8c375d3581af47d2f8a2d448e5f05047176d1
nick <nick@runwithitsynthetics.com>
2018-07-12 12:13:07 -0700

    Adding initial files and work. Disk Test does not work yet.

---
+++ disk_test.sh
@@ -0,0 +1,96 @@
+!/bin/bash -x
+
+# NOTES AND GENERAL INFO
+# REQUIRES PSSH (Parallel SSH)
+# sudo apt install pssh
+
+# CONFIGURABLES
+testSize="100M"
+
+# Hosts are defined in the pssh-hosts file
+
+mkdir -p logs
+
+echo "Starting Disk Test ... "
+echo ""
+
+filename=pssh-hosts
+MACHINES=""
+totalMachines=0
+
+# Read pssh-hosts in
+while read -r line; do
+	let "totalMachines++"
+	MACHINES="$MACHINES $line"
+done < "$filename"
+
+# If you want to only run a local test, add the -l flag.
+if [ "$1" == "-l" ]; then
+	# Only run locally
+	echo "Running Local Disk Test ... "
+	echo ""
+	echo "Results:"
+	fio -randrepeat=1 -ioengine=libaio -direct=1 -gtod_reduce=1 -name=test -filename=test -iodepth=64 -size=$testSize --readwrite=randrw --rwmixread=75 | awk '/sda/' > logs/LocalMachine.log
+	echo ""
+	echo "... Done"
+	exit 1
+fi
+
+# If local flag is not present, then perform networked disk test.
+if [ "$1" != "-l" ]; then
+	echo "Running Networked Disk Test ... "
+	echo ""
+	echo "Participating Servers:"
+	for MACHINE in $MACHINES ; do
+		echo $MACHINE
+	done
+	echo "TOTAL: $totalMachines"
+	echo ""
+
+	# Using PSSH navigate to benchmarking directory,
+	# do fio test
+	# then scp resulting log file back to ~/Desktop/benchmarking directory.
+	pssh -t 300 -O StrictHostKeyChecking=no -h pssh-hosts -l invidi -A -o '`hostname.log`' 'mkdir -p benchmarking; cd benchmarking; fio -randrepeat=1 -ioengine=libaio -direct=1 -gtod_reduce=1 -name=test -filename=test -iodepth=64 -size=$testSize --readwrite=randrw --rwmixread=75 | awk "/sda/" > `hostname`.log'
+
+	# for each MACHINE in MACHINES, scp the .log file back
+	for MACHINE in $MACHINES ; do
+		echo ""
+		echo "SCP log from $MACHINE"
+		scp "invidi@$MACHINE":~/benchmarking/*.log ~/Desktop/benchmarking/logs/$MACHINE.log
+		echo ""
+	done
+
+	# Afterwards, do a local test.
+	# Test Sequential Read/Write (75% read) on local machine.
+	fio -randrepeat=1 -ioengine=libaio -direct=1 -gtod_reduce=1 -name=test -filename=test -iodepth=64 -size=$testSize --readwrite=randrw --rwmixread=75 | awk '/sda/' > logs/LocalMachine.log
+fi
+
+printf "\n\nSUMMARY"
+
+# PRINTING RESULTS
+printf "\n`hostname`\n"
+local_test_result=`awk -F',' '{ print }' logs/LocalMachine.log`
+printf "%s\n" "$local_test_result"
+
+for MACHINE in $MACHINES ; do
+	printf "\n$MACHINE\n"
+	test_result=`awk -F',' '{ print }' logs/$MACHINE.log`
+	printf "%s\n" "$test_result"
+done
+
+echo ""
+echo "... Done"
+# hostname notes
+# expanse-bdms.invidi.com has different login credentials
+# expanse-etl.invidi.com
+# expanse-etl2.invidi.com
+# expanse-jetty.invidi.com
+# expanse-jetty2.invidi.com
+# expanse-metrics.invidi.com
+# expanse-nlb.invidi.com
+# expanse-cas1.invidi.com
+# expanse-cas2.invidi.com
+# expanse-cas3.invidi.com
+# expanse-cas4.invidi.com
+# expanse-cas5.invidi.com
+# expanse-cas6.invidi.com
---
+++ net_test.sh
@@ -0,0 +1,71 @@
+#!/bin/bash -x
+
+# Path to qperf
+qperf=qperf-0.4.9/src/qperf
+
+# Checking for install of qperf
+if [ -z $qperf ] ; then
+	echo "qperf needs to be isntalled!"
+	exit 1
+fi
+
+# Handle too many args error (should only accept 0 or 1 args.)
+if [ $# -gt 1 ]; then
+	printf "Too Many Input Arguments ... \n"
+	printf "Enter 0 arguments for server mode, or an IP address.\n"
+	exit 1
+fi
+
+# CONFIGURABLES
+
+# Provide more detailed output (Default true)
+verbose=true
+
+# Set how long to run the test (Default 30 seconds)
+time=30
+
+# Set timeout. This is the timeout used for various things
+# such as exchanging messages. (Default 5 seconds)
+timeout=5
+
+# Set port for socket tests (Default 19865)
+socketPort=19865
+
+# Set port for listening (Default 19765)
+listenPort=19765
+
+# qperf uses port 19765 by default for listening.
+# This must be set to the same port on both the server and client machines.
+
+
+# ADDING PORTS TO FIREWALL FOR 1 HOUR
+printf "\nAdding port 19765 for 1 hour ... "
+sudo firewall-cmd --add-port=19765/tcp --timeout=1h
+printf "\nAdding port 19865 for 1 hour ... "
+sudo firewall-cmd --add-port=19865/tcp --timeout=1h
+
+# When the script is run in server mode, the device begins to listen
+# for other devices to begin a test.
+
+# Example: Cassandra1 could run in server mode and on Cassandra 2
+# you can add the argument which is the IP address of Cassandra 1.
+# This would test the connection between Cassandra1 and Cassandra 2.
+
+
+if [ $# -eq 0 ]; then
+	# IF THERE IS NO INPUT ARGS, USE SERVER MODE
+	printf "\nStarting qperf in server mode ...\n"
+	qperf-0.4.9/src/qperf
+elif [ $# -eq 1 ]; then
+	# IF THERE IS 1 INPUT ARG, USE QPERF NORMALLY.
+	IP=$1
+	printf "\n\nRunning qperf on IP: $1 ...\n\n"
+	# IF VERBOSE IS TRUE, SHOW DETAILED OUTPUT
+	if [ $verbose ]; then
+		$qperf -to $timeout -t $time -v -ip $socketPort -lp $listenPort $IP tcp_bw tcp_lat | net_test_results.log
+	else
+		$qperf -to $timeout -t $time -ip $socketPort -lp $listenPort $IP tcp_bw tcp_lat | tee net_test_results.log
+	fi
+fi
+
+printf "\n... Done. Output also availible in benchmarking/net_test_results.log\n\n"
