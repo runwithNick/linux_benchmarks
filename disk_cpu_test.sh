da55ced3b83090000c0b5f8bd3fdecbb2b5d45bf
nick <nick@runwithitsynthetics.com>
2018-07-19 12:18:37 -0700

    disk_cpu_test.sh is fully equipped and ready for the real benchmarking faze.

--- disk_cpu_test.sh
+++ disk_cpu_test.sh
@@ -26,6 +26,7 @@ trap 'err_handler $LINENO' ERR
 mkdir -p logs
 mkdir -p logs/seekerLogs
 mkdir -p logs/cpuLogs
+mkdir -p logs/fioLogs
 mkdir -p logs/errors
 mkdir -p logs/errors/parallel-scp
 mkdir -p logs/errors/pssh
@@ -47,7 +48,7 @@ echo ""
 # make directories
 echo "Making directories ..."
 #pssh -e "logs/errors/pssh" -i -t 300 -O StrictHostKeyChecking=no -h pssh-hosts -l $RUNAS -A 'HOSTNAME=`hostname` ; mkdir -p benchmarking; cd benchmarking'
-pssh -e "logs/errors/pssh" -i -t 300 -O StrictHostKeyChecking=no -h pssh-hosts -l $RUNAS -A 'HOSTNAME=`hostname` ; mkdir -p benchmarking; cd benchmarking'
+pssh -e "logs/errors/pssh" -i -t 900 -O StrictHostKeyChecking=no -h pssh-hosts -l $RUNAS -A 'HOSTNAME=`hostname` ; mkdir -p benchmarking; cd benchmarking'
 echo "... Done"
 echo ""
 
@@ -61,8 +62,8 @@ echo ""
 # do fio test
 # then scp resulting log file back to logs directory.
 # do seeker test, then get log back to logs/seekerLogs
-echo "Install sysbench then -> Running fio, seeker, and sybench cpu tests on machines ... "
-pssh -e "logs/errors/pssh" -i -t 300 -O StrictHostKeyChecking=no -h pssh-hosts -l $RUNAS -A 'sudo yum -y install sysbench; HOSTNAME=`hostname` ; cd benchmarking; fio -randrepeat=1 -ioengine=libaio -direct=1 -gtod_reduce=1 -name=test -filename=test -iodepth=64 -size=100M --readwrite=randrw --rwmixread=75 > ${HOSTNAME}.log; mkdir -p seekerLogs; sudo ./seeker /dev/sda > seekerLogs/${HOSTNAME}.log; numCores=`grep -c ^processor /proc/cpuinfo`; mkdir -p cpuLogs; sysbench --test=cpu --num-threads="$numCores" run > cpuLogs/${HOSTNAME}.log'
+echo "Install sysbench, and fio then -> Running fio, seeker, and sybench cpu tests on machines ... "
+pssh -e "logs/errors/pssh" -i -t 300 -O StrictHostKeyChecking=no -h pssh-hosts -l $RUNAS -A 'sudo yum -y install sysbench; sudo yum -y install fio; HOSTNAME=`hostname`; cd benchmarking; mkdir -p fioLogs; fio -randrepeat=1 -ioengine=libaio -direct=1 -gtod_reduce=1 -name=test -filename=test -iodepth=64 -size=100M --readwrite=randrw --rwmixread=75 > fioLogs/${HOSTNAME}.log; mkdir -p seekerLogs; sudo ./seeker /dev/sda > seekerLogs/${HOSTNAME}.log; numCores=`grep -c ^processor /proc/cpuinfo`; mkdir -p cpuLogs; sysbench --test=cpu --num-threads="$numCores" run > cpuLogs/${HOSTNAME}.log'
 echo "... Done"
 echo ""
 
@@ -70,7 +71,7 @@ echo ""
 for MACHINE in $MACHINES ; do
     echo ""
     echo "SCP fio log from $MACHINE"
-    scp -r "${RUNAS}@$MACHINE":benchmarking/*.log logs/$MACHINE.log
+    scp -r "${RUNAS}@$MACHINE":benchmarking/fioLogs/*.log logs/fioLogs/$MACHINE.log
     echo "SCP seeker log from $MACHINE"
     scp -r "${RUNAS}@${MACHINE}":benchmarking/seekerLogs/*.log logs/seekerLogs/$MACHINE.log
 		echo "SCP cpu log from $MACHINE"
@@ -87,7 +88,7 @@ for MACHINE in $MACHINES ; do
 	printf "\n$MACHINE\n"
 	echo ""
 	echo "   *** fio ***"
-	cat logs/$MACHINE.log
+	cat logs/fioLogs/$MACHINE.log
 	echo ""
 	echo "   *** seeker ***"
 	cat logs/seekerLogs/$MACHINE.log
